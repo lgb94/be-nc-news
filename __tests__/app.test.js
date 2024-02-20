c5d77480feaf9303eadf1301f7fd1b7298dc430b
lgb94 <lgb_94@hotmail.com>
2024-02-20 13:51:44 +0000

    task 5 completed - sql-query tester and results file created for SQL query construction

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -53,7 +53,7 @@ describe('GET api/articles/:article_id', () => {
                 expect(body.article).toMatchObject({
                     "author": expect.any(String),
                     "title": expect.any(String),
-                    "article_id": expect.any(Number),
+                    "article_id": 1,
                     "body": expect.any(String),
                     "created_at": expect.any(String),
                     "votes": expect.any(Number),
@@ -75,6 +75,60 @@ describe('GET api/articles/:article_id', () => {
             .expect(400)
             .then(({body}) => {
                 expect(body.msg).toBe("bad request")
+            })
+    })
+});
+
+describe('GET api/articles', () => {
+    test('sending an API request to api/articles returns a Status-200: responds with an object, with a key articles, which has the value of an array of ALL articles, in this case - 13 total.', () => {
+        return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then(({body}) => {
+                const articles = body.articles
+                expect(articles).toHaveLength(13)
+            })
+    })
+    test('sending an API request to api/articles returns a Status-200: responds with an object, with a key articles, which has the value of an array of ALL articles, with the correct keys assigned', () => {
+        return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then(({body}) => {
+                const articles = body.articles
+                articles.forEach((article) => {
+                    expect(article).toMatchObject({
+                        "author": expect.any(String),
+                        "title": expect.any(String),
+                        "article_id": expect.any(Number),
+                        "topic": expect.any(String),
+                        "created_at": expect.any(String),
+                        "votes": expect.any(Number),
+                        "article_img_url": expect.any(String),
+                        "comment_count": expect.any(String)
+                    })
+                })
+            })
+    })
+    test('sending an API request to api/articles returns a Status-200: responds with an object, with a key articles, which has the value of an array of ALL articles, with the correct keys assigned, and is sorted by created_at in descending order', () => {
+        return request(app)
+            .get('/api/articles')
+            .expect(200)
+            .then(({body}) => {
+                const articles = body.articles
+                articles.forEach((article) => {
+                    expect(article).toMatchObject({
+                        "author": expect.any(String),
+                        "title": expect.any(String),
+                        "article_id": expect.any(Number),
+                        "topic": expect.any(String),
+                        "created_at": expect.any(String),
+                        "votes": expect.any(Number),
+                        "article_img_url": expect.any(String),
+                        "comment_count": expect.any(String)
+                    })
+                })
+                expect(articles[0].created_at).toBe('2020-11-03T09:12:00.000Z')
+                expect(articles[12].created_at).toBe('2020-01-07T14:08:00.000Z')
             })
     })
 });
